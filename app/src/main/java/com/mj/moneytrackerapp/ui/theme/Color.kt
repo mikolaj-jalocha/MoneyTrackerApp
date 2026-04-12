941e3c9c64aa36b2d1dbe4310adbea2e87fae81b
mikolaj-jalocha <mikics2k17@gmail.com>
2026-04-12 10:15:04 +0200

    init: initial commit

---
+++ app/src/main/java/com/mj/moneytrackerapp/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.mj.moneytrackerapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightGreen = Color(0xFFDFF7E2)
+
+val MainGreen = Color(0xFF00D09E)
+
+val BackgroundGreen = Color(0xFFF1FFF3)
+
+val DarkModeGreenBar = Color(0xFF0E3E3E)
+
+val LettersAndIcons = Color(0xFF093030)
